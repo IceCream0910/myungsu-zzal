a3a35e7362493272fffc489740dc1029ee310a95
Icecream0910 <taein2370@naver.com>
2022-11-20 22:08:09 +0900

    UPDATE: 무한 스크롤 적용

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ $(function () {
     Kakao.init('cf575f9210b7ec64665aecad9bbcbbab');
 
     //초기 세팅
-    //배열에서 값 랜덤 추출
     const defaultKeywords = ['분노', '슬픔', '우울', '귀찮', '피곤']
     var random = Math.floor(Math.random() * defaultKeywords.length);
     $.ajax({
@@ -35,11 +34,14 @@ $(function () {
 });
 
 var imgLink = '';
-
+var lastQuery = '';
+var lastIndex = 0;
 
 function submit() {
     const text = $('#text-input').val();
     if (text) {
+        lastQuery = text;
+        lastIndex = 0;
         $.ajax({
             url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCSJZaed6FebVZm2mEqbeIeHyspQfHKjwI&cx=35cc76baf4e73d7f8&searchType=image&q=박명수 짤 ' + text,
             type: 'GET',
@@ -61,6 +63,9 @@ function submit() {
             `);
                     }
                 }
+                $('html, body').animate({
+                    scrollTop: $('html').offset().top
+                }, 500);
 
             } else {
                 alert('검색 결과가 없습니다.');
@@ -70,11 +75,63 @@ function submit() {
 
 }
 
+
+function loadMore() {
+    const text = $('#text-input').val();
+    if (text) {
+        $.ajax({
+            url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCSJZaed6FebVZm2mEqbeIeHyspQfHKjwI&cx=35cc76baf4e73d7f8&searchType=image&q=박명수 짤 ' + lastQuery + '&start=' + lastIndex,
+            type: 'GET',
+        }).done(function (response) {
+            console.log(response.items[0].link);
+            if (response) {
+                for (var i = 0; i < response.items.length; i++) {
+                    if (response.items[i].fileFormat != 'image/') { //이미지 아님
+                        $('#result').append(`
+                <figure class="post-image" onclick="updateImgUrl('${response.items[i].link}');" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="${response.items[i].link}" itemprop="contentUrl" data-size="${response.items[i].image.width}x${response.items[i].image.height}">
+                    <img src="${response.items[i].link}" itemprop="thumbnail" onerror="this.style.display='none'"  style="width: 100%;
+                        border-radius: 20px;
+                        object-fit: cover;" />
+                </a>
+                <figcaption itemprop="caption description"></figcaption>
+            </figure>
+            `);
+                    }
+                }
+            } else {
+                alert('검색 결과가 없습니다.');
+            }
+        });
+    }
+
+}
+
+
 $('#text-input').on('keypress', function (e) {
     if (e.keyCode === 13) {
         submit();
     }
 });
+
+
+const Element = document.querySelector('.bottomOfList')
+const options = {}
+// observer: IntersectionObserver instance
+const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && $('#result').html().length > 5) {
+            if (lastQuery) {
+                lastIndex += 10;
+                loadMore();
+            }
+        }
+    })
+}, options)
+
+io.observe(Element)
+
+
 
 function updateImgUrl(url) {
     imgLink = url;
